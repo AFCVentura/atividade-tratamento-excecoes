9959fbdf6c0b0ea936e5ac6a6a9bd13387e66ca5
Ventura <ventura.py3@gmail.com>
2024-06-26 15:47:34 -0300

    versao com excecoes nao tratadas

---
+++ src/Average.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp1
+{
+    public class Average
+    {
+        public int CalcularMedia(int quant, int[] numbers) {
+            int result = 0;
+            foreach (var number in numbers) 
+            {
+                result += number;
+            }
+            
+            return result / quant;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Average calc = new Average();
+            Console.WriteLine("Você quer calcular a média de quantos números?");
+            int quant = int.Parse(Console.ReadLine());
+            int[] vetor = new int[quant];
+
+            for (int i = 0; i >= 0; i++) {
+                Console.WriteLine($"Digite o {i+1}º número: ");
+                int n1 = int.Parse(Console.ReadLine());
+                vetor[i] = n1;
+
+                System.Console.WriteLine("Quer digitar mais um número? (s/n)");
+                System.Console.WriteLine($"Tamanho do vetor: {quant}");
+                System.Console.WriteLine($"Números digitados: {i + 1}");
+
+                if (Console.ReadLine().ToUpper() == "S") {
+                    continue;
+                }
+                else if (Console.ReadLine().ToUpper() == "N") {
+                    break;
+                }
+                else {
+                    System.Console.WriteLine("Digite um valor válido");
+                    continue;
+                }
+            }
+            Console.WriteLine(calc.CalcularMedia(quant, vetor));
+            
+        }
+    }
+}
