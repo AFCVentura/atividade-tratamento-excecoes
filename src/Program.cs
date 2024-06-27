6bf792558e5fec8571b512aa35d7efdac6d9f056
João <joaomoissapersonal@Gmail.com>
2024-06-27 16:24:01 -0300

    minha parte

--- src/Program.cs
+++ src/Program.cs
@@ -1,36 +1,51 @@
 ﻿
+using System.Linq.Expressions;
+
 namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Average calc = new Average();
-            Console.WriteLine("Você quer calcular a média de quantos números?");
-            int quant = int.Parse(Console.ReadLine());
-            int[] vetor = new int[quant];
+            
+                Average calc = new Average();
+                Console.WriteLine("Você quer calcular a média de quantos números?");
+                int quant = int.Parse(Console.ReadLine());
+                int[] vetor = new int[quant];
+            try
+            {
+                for (int i = 0; i >= 0; i++)
+                {
+                    Console.WriteLine($"Digite o {i + 1}º número: ");
+                    int n1 = int.Parse(Console.ReadLine());
+                    vetor[i] = n1;
 
-            for (int i = 0; i >= 0; i++) {
-                Console.WriteLine($"Digite o {i+1}º número: ");
-                int n1 = int.Parse(Console.ReadLine());
-                vetor[i] = n1;
+                    System.Console.WriteLine("Quer digitar mais um número? (s/n)");
+                    System.Console.WriteLine($"Tamanho do vetor: {quant}");
+                    System.Console.WriteLine($"Números digitados: {i + 1}");
 
-                System.Console.WriteLine("Quer digitar mais um número? (s/n)");
-                System.Console.WriteLine($"Tamanho do vetor: {quant}");
-                System.Console.WriteLine($"Números digitados: {i + 1}");
 
-                if (Console.ReadLine().ToUpper() == "S") {
-                    continue;
+                    if (Console.ReadLine().ToUpper() == "S")
+                    {
+                        continue;
+                    }
+                    else if (Console.ReadLine().ToUpper() == "N")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Digite um valor válido");
+                        continue;
+                    }
                 }
-                else if (Console.ReadLine().ToUpper() == "N") {
-                    break;
-                }
-                else {
-                    System.Console.WriteLine("Digite um valor válido");
-                    continue;
-                }
+
+                Console.WriteLine(calc.CalcularMedia(quant, vetor));
             }
-            Console.WriteLine(calc.CalcularMedia(quant, vetor));
+            catch (Exception ex)
+            {
+                Console.WriteLine("Esse número é incompatível");
+            }
             
         }
     }
