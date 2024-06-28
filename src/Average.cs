082c07b7feccf2cb028e893194ae671877370841
Luigi <luigicumaru@gmail.com>
2024-06-28 12:20:11 -0300

    Primeira Alteração

--- src/Average.cs
+++ src/Average.cs
@@ -2,13 +2,14 @@ namespace ConsoleApp1
 {
     public class Average
     {
-        public int CalcularMedia(int quant, int[] numbers) {
+        public int CalcularMedia(int quant, int[] numbers)
+        {
             int result = 0;
-            foreach (var number in numbers) 
+            foreach (var number in numbers)
             {
                 result += number;
             }
-            
+
             return result / quant;
         }
     }
